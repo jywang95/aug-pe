8e701d8c17bd0b2b54319c7a0f1395f611755329
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-09-23 15:53:43 -0400

    hf offline

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fl
 
 export CUDA_VISIBLE_DEVICES=0 
-
+export HF_HUB_OFFLINE = 1
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
