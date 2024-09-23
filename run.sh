8f5f949a997f8bf4001a46ff386d0a68fb6d6ff4
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-09-23 14:28:39 -0400

    add

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A MED128
+#SBATCH -J test
+#SBATCH -p gpu_acmhs
+#SBATCH -N 1
+#SBATCH -t 00:20:00
+#SBATCH -o ./%j-multithread-output.txt
+#SBATCH -e ./%j-multithread-error.txt
+
+
+
+# source activate /gpfs/wolf2/olcf/stf007/8i9/jupyter_env
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate fl
+export CUDA_VISIBLE_DEVICES=0 
+
+srun bash ./scripts/embeddings.sh --yelp  
