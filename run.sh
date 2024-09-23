ac7c3de9716bed4b7dba2b82ff21564d9b456d5c
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-09-23 14:41:47 -0400

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH -J test
 #SBATCH -p gpu_acmhs
 #SBATCH -N 1
-#SBATCH -t 00:20:00
+#SBATCH -t 00:30:00
 #SBATCH -o ./%j-multithread-output.txt
 #SBATCH -e ./%j-multithread-error.txt
 
@@ -13,6 +13,7 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fl
-export CUDA_VISIBLE_DEVICES=0 
+
+nvidia-smi
 
 srun bash ./scripts/embeddings.sh --yelp  
