50ffaec9742d04dfa9c8446e1caa1fb57126be38
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-09-23 15:59:13 -0400

    debug

--- run.sh
+++ run.sh
@@ -15,9 +15,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fl
 
 export CUDA_VISIBLE_DEVICES=0 
-export HF_HUB_OFFLINE = 1
-export TRANSFORMERS_OFFLINE=1
-export HF_DATASETS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+
 
 
 srun bash scripts/hf/yelp/generate.sh 
