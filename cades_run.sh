88e173e30046ba4ee8d06fc147bd2345a0bdd2f0
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-10-03 23:35:21 -0400

    debug

---
+++ cades_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -A birthright
+#SBATCH -p burst
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -J multithread-test-job
+#SBATCH --mem=32g
+#SBATCH -t 60:00
+#SBATCH -o ./%j-multithread-output.txt
+#SBATCH -e ./%j-multithread-error.txt
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=w7j@ornl.gov
+##SBATCH -q debug
+
+srun bash scripts/hf/yelp/generate.sh
