76e5a7d4855412b0cc999bc38894e284c3f03b0b
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-10-03 23:38:55 -0400

    add conda env

--- cades_run.sh
+++ cades_run.sh
@@ -8,11 +8,14 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH -J multithread-test-job
 #SBATCH --mem=32g
-#SBATCH -t 60:00
+#SBATCH -t 00:60:00
 #SBATCH -o ./%j-multithread-output.txt
 #SBATCH -e ./%j-multithread-error.txt
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=w7j@ornl.gov
 ##SBATCH -q debug
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate FedLLM
 
 srun bash scripts/hf/yelp/generate.sh
