d24722426357ed9d773eab3710a93e86cca1d1b8
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-10-06 21:59:05 -0400

    generate data

--- run.sh
+++ run.sh
@@ -15,9 +15,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fl
 
-srun bash scripts/embeddings.sh --yelp  
 
 
-
-# srun bash scripts/hf/yelp/generate.sh 
+srun bash scripts/hf/yelp/generate.sh 
 
